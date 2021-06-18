7b3eb6c4f162e10086da29d74c971f2b93cee25f
Louis Savoie <thelousavoie@gmail.com>
2021-06-18 07:55:22 -0400

    add inventory file read/write and new method

--- app.rb
+++ app.rb
@@ -1,3 +1,25 @@
+# Requires
+require 'yaml'
+
+# Global Variables Initializations
+$inventory = Hash.new
+
+# Write Inventory to YAML File
+def write_to_yaml
+  File.open("inventory.yml", "w") { |file| file.write($inventory.to_yaml) }
+end
+
+# Load Inventory
+begin
+  puts "Loading inventory"
+  $inventory = YAML.load(File.read("inventory.yml"))
+rescue Errno::ENOENT
+  puts "No inventory file found, starting a new inventory"
+  write_to_yaml
+else
+  puts "Inventory loaded"
+end
+
 # Help method
 def help
   puts <<~HELP
@@ -5,8 +27,18 @@ def help
     ----------------------------------------------
     help / displays this information
     new / creates a new item in the database
-    end / saves the database and exits the program
+    exit / saves the database and exits the program
   HELP
+end
+
+# New method
+def new
+  print "Item name: "
+  name = gets.chomp.downcase
+  print "Current on-hand: "
+  current = gets.chomp.downcase
+  $inventory[name] = current
+  write_to_yaml
 end
 
 # Starting messages
@@ -18,14 +50,14 @@ while true
   command = gets.chomp.downcase
 
   case command
-  when "end"
+  when "exit"
     puts "Saving"
     puts "Exiting"
     break
   when "help"
     help
   when "new"
-    puts "TEST: insert new method here"
+    new
   else
     puts "ERROR: Command unknown"
   end
